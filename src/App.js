c1b3bbe7c3a9e46126bb2465de30ea4873e666ea
Vasylyna Lykhogodenko <vasilisapj89@gmail.com>
2022-10-20 23:20:53 +0300

    add logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
+import logo from "./logo.png";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header>
+        <img className="logo-image" src={logo} alt="dictionary logo" width={100} />
         <h1>Dictionary App</h1>
       </header>
     </div>
