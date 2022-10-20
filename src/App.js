322166509a57011561eb8d6c19f824d45d3f78b1
Vasylyna Lykhogodenko <vasilisapj89@gmail.com>
2022-10-21 01:18:08 +0300

    add form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import logo from "./logo.png";
+import { AiOutlineSearch } from 'react-icons/ai';
 import './App.css';
 
 function App() {
@@ -7,8 +8,17 @@ function App() {
     <div className="App">
       <header>
         <img className="logo-image" src={logo} alt="dictionary logo" width={100} />
-        <h1>Dictionary App</h1>
+        <h1>Welcome to the Magic Dictionary</h1>
       </header>
+      <form>
+        <div>
+          <label for="search-word"></label>
+          <input id="search-word" type="search" placeholder="Search for a word" />
+          <button>
+            <AiOutlineSearch />
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
