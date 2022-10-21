dcc52e083c50d4dc88763e696a60b6dc5dcd8b34
codingmemories <you@example.com>
2022-10-21 23:15:48 +0300

    add phonetic audio

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Meaning from "../Meaning/Meaning";
+import Phonetic from "../Phonetic/Phonetic";
 
 import img from "../../images/magic-background.gif";
 import "./results.css";
@@ -12,6 +13,13 @@ export default function Results(props) {
     return (
       <div className="results">
         <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
