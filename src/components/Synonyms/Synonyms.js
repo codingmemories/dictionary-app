195398619fec2985da0e7b37ca315f8bf412c2c9
codingmemories <you@example.com>
2022-10-21 22:26:43 +0300

    add synonyms

---
+++ src/components/Synonyms/Synonyms.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  console.log(props.synonyms);
+
+  if (props.synonyms) {
+    return (
+      <ul className="synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>;
+        })}
+      </ul>
+    );
+  } else {
+    return null;
+  }
+}
